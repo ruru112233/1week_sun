0ff6398c7778c710e2d86810d016f8c363e26891
ruru112233 <esxwaz1234@gmail.com>
2021-08-08 00:18:05 +0900

    水滴の自動生成機能追加

---
+++ Assets/Scripts/ObjPoolGen.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SearchService;
+
+public class ObjPoolGen : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject smallMeteoPool = null
+                     , nomalMeteoPool = null
+                     , bigMeteoPool = null
+                     , rightDropPool = null
+                     , leftDropPool = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("GetRightDrop", 0.0f, 1.0f);
+        InvokeRepeating("GetLeftDrop", 0.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // âEâÒì]ÇÃêÖìHÇê∂ê¨
+    void GetRightDrop()
+    {
+        foreach (Transform t in rightDropPool.transform)
+        {
+            if (!t.gameObject.activeSelf)
+            {
+                t.SetPositionAndRotation(InstancePoint(), Quaternion.identity);
+                t.gameObject.SetActive(true);
+                return;
+            }
+        }
+
+        GameObject obj = rightDropPool.transform.GetChild(0).gameObject;
+        Instantiate(obj, InstancePoint(), Quaternion.identity, rightDropPool.transform);
+    }
+
+    // ç∂âÒì]ÇÃêÖìHÇê∂ê¨
+    void GetLeftDrop()
+    {
+        foreach (Transform t in leftDropPool.transform)
+        {
+            if (!t.gameObject.activeSelf)
+            {
+                t.SetPositionAndRotation(InstancePoint(), Quaternion.identity);
+                t.gameObject.SetActive(true);
+                return;
+            }
+        }
+
+        GameObject obj = leftDropPool.transform.GetChild(0).gameObject;
+        Instantiate(obj, InstancePoint(), Quaternion.identity, leftDropPool.transform);
+    }
+
+    // ê∂ê¨à íuÇÃéZèo
+    Vector3 InstancePoint()
+    {
+        Vector3 pos = new Vector3();
+
+        // óêêîÇÃê›íË
+        float x = Random.Range(80.0f, 500.0f);
+        float y = Random.Range(80.0f, 500.0f);
+        float z = Random.Range(80.0f, 500.0f);
+
+        bool checkX = false;
+        bool checkY = false;
+        bool checkZ = false;
+        int xx = 0;
+        int yy = 0;
+        int zz = 0;
+        // ê≥ïâÇÃê›íË
+        while (!checkX)
+        {
+            xx = Random.Range(-1, 2);
+            if (xx != 0) checkX = true;
+        }
+        while (!checkY)
+        {
+            yy = Random.Range(-1, 2);
+            if (yy != 0) checkY = true;
+        }
+        while (!checkZ)
+        {
+            zz = Random.Range(-1, 2);
+            if (zz != 0) checkZ = true;
+        }
+
+        // xç¿ïWÇÃê›íË
+        x = x * xx;
+        // yç¿ïWÇÃê›íË
+        y = y * yy;
+        // zç¿ïWÇÃê›íË
+        z = z * zz;
+
+        pos = new Vector3(x, y, z);
+
+        return pos;
+    }
+}
